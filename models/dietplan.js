6897156edc28f9afcdcc16787c09fcfda7e6c808
mohammad-hakeem <153605047+mohammad-hakeem@users.noreply.github.com>
2026-04-19 23:51:56 +0300

    Update dietplan.js

--- models/dietplan.js
+++ models/dietplan.js
@@ -22,7 +22,7 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false
     }
   }, {
-    timestamps: true // ✅ هنا المكان الصحيح
+    timestamps: true 
   });
 
   DietPlan.associate = function(models) {
